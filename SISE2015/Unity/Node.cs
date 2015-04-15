6aa54536f66c413303b9843c6b6858aed43f6511
Luke Spierewka <spierek@gmail.com>
2015-04-15 15:24:21 +0200

    [+] Unity graph generator prototype

---
+++ SISE2015/Unity/Node.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+    #region Variables
+    public Vector3 position;
+    public List<Node> neighbors;
+    #endregion
+
+    #region Constructors
+    public Node(Vector3 pos) {
+        position = pos;
+        neighbors = new List<Node>();
+    }
+    #endregion
+
+    #region Methods
+    public void AddNeighbor(Node neighbor) {
+        neighbors.Add(neighbor);
+    }
+
+    public void VisualizeNode(float radius) {
+        Gizmos.DrawSphere(position, radius);
+    }
+
+    public void VisualizeConnections() {
+        foreach (Node n in neighbors) {
+            Gizmos.DrawLine(position, n.position);
+        }
+    }
+    #endregion
+}
