7eeb0aed7a097a57477f1f430087acd6ed1f1640
Luke Nizik <elejdor@gmail.com>
2015-04-16 18:32:13 +0200

    [+] Command interface

--- SISE2015/SISE2015/GameController.h
+++ SISE2015/SISE2015/GameController.h
@@ -1,8 +1,16 @@
 #pragma once
+#include "Pawn"
+#include "Player.h"
+
 class GameController
 {
 private:
-
+	struct PlayerInfo
+	{
+		bool currentlyMoving = false;
+		Pawn* pawn;
+		Player* player;
+	};
 public:
 	GameController();
 	~GameController();
