8da471f23c0652e790b0e32360cad52ac89c7010
MatushkaRossiya <matushkarossiya@email.ru>
2015-04-17 00:33:52 +0200

    Stats WIP.

--- SISE2015/SISE2015/Stats.cpp
+++ SISE2015/SISE2015/Stats.cpp
@@ -1,2 +1,15 @@
 #include "Stats.h"
 
+Stats::Stats(std::string filename)
+{
+	file.open(filename);
+}
+
+Stats::~Stats()
+{
+	if (file.is_open())
+	{
+		file.flush();
+		file.close();
+	}
+}
--- SISE2015/SISE2015/Stats.h
+++ SISE2015/SISE2015/Stats.h
@@ -1,9 +1,30 @@
 #ifndef _STATS_H_
 #define _STATS_H_
 
+#include <fstream>
+#include <map>
+#include <string>
+#include "Player.h"
+
 class Stats
 {
+public:
+	Stats(std::string filename);
+	~Stats();
 
+private:
+	struct PerPlayerStats
+	{
+		int kills;
+		int deaths;
+		int survival;
+		float processingTimeSum;
+		float processingTimeAvg;
+		int turns;
+	};
+
+	std::map<Player*, PerPlayerStats*> stats;
+	std::ofstream file;
 };
 
 #endif
