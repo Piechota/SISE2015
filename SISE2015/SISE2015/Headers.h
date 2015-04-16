ff4ff1876bf9b9762fdb804fa4f0be47ea5d4031
Filip Wróbel <wrobelfilip@gmail.com>
2015-04-16 18:32:47 +0200

    [+] Pawn class [+]Example Pawn class [+] Example loop with pawns in main.cpp

--- SISE2015/SISE2015/Headers.h
+++ SISE2015/SISE2015/Headers.h
@@ -4,11 +4,18 @@
 #include <iostream>
 #include <math.h>
 #include <SDL.h>
+#include <ctime>
 
 #include "Globals.h"
 #include "Color.h"
 #include "Renderer.h"
 
+#include "Pawn.h"
+#include "ExamplePawn.h"
+
 using namespace std;
+
+const int windowWidth = 700;
+const int windowHeight = 700;
 
 #endif
